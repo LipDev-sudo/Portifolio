ccea5e33b8b0b8b770410f43ce46b56870d8eac2
Hamilton <hamiltonfelipe019@gmail.com>
2026-03-07 16:29:35 -0300

    corrigindo rotas

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,16 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
 
-
 def home(request):
     return HttpResponse("API do Portfólio funcionando")
 
-
 urlpatterns = [
     path('', home),
-
     path('admin/', admin.site.urls),
-
     path('api/projetos/', include('projetos.urls')),
     path('api/servicos/', include('servicos.urls')),
     path('api/contato/', include('contato.urls')),
