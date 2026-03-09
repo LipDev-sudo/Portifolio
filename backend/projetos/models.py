77ecc2e1cfcd304b2c86443282d9c138f0d27678
Hamilton <hamiltonfelipe019@gmail.com>
2026-03-09 18:34:12 -0300

    update image field

--- backend/projetos/models.py
+++ backend/projetos/models.py
@@ -5,7 +5,7 @@ class Projeto(models.Model):
     descricao = models.TextField()
     tecnologias = models.CharField(max_length=200)
     link_projeto = models.URLField(blank=True, null=True)
-    imagem = models.ImageField(upload_to='projetos/', blank=True, null=True)
+    imagem = models.URLField(blank=True, null=True)
     criado_em = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
