85b80c0fa9bdab23eda114bb4ac8fb5bad752177
Hamilton <hamiltonfelipe019@gmail.com>
2026-03-09 18:06:58 -0300

    fix media files serving

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 def home(request):
     return HttpResponse("API do Portfólio funcionando")
+
 
 urlpatterns = [
     path('', home),
@@ -12,3 +17,6 @@ urlpatterns = [
     path('api/servicos/', include('servicos.urls')),
     path('api/contato/', include('contato.urls')),
 ]
+
+# servir arquivos de mídia (imagens)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
